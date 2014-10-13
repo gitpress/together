16265ebbb4ad01ac20371a1641f3fad188e2a13f
anotherangrymouse <grillopress.com@gmail.com>
2014-10-13 16:01:31 +0000

    add twitter and home trust section to front page template

--- wp-content/themes/wordpress-bootstrap-master/page-homepage.php
+++ wp-content/themes/wordpress-bootstrap-master/page-homepage.php
@@ -27,6 +27,44 @@ Template Name: Homepage
   </div>
 </div>
 
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <h2>Participating Trusts</h2>
+      <hr>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      <div class="sthbrand">
+        <h4 class="lighttext"><a href="#" class="lighttext">Sheffield Teaching Hospitals NHS Foundation Trust</a></h4>
+      </div>
+      <div class="doncasterbrand">
+        <h4 class="lighttext"><a href="#" class="lighttext">Sheffield Teaching Hospitals NHS Foundation Trust</a></h4>
+      </div>
+      <div class="chesterfieldbrand">
+        <h4 class="lighttext"><a href="#" class="lighttext">Sheffield Teaching Hospitals NHS Foundation Trust</a></h4>
+      </div>
+      <div class="rotherhambrand">
+        <h4 class="lighttext"><a href="#" class="lighttext">Sheffield Teaching Hospitals NHS Foundation Trust</a></h4>
+      </div>
+      <div class="sheffchildbrand">
+        <h4 class="lighttext"><a href="#" class="lighttext">Sheffield Teaching Hospitals NHS Foundation Trust</a></h4>
+      </div>
+      <div class="midyorkbrand">
+        <h4 class="lighttext"><a href="#" class="lighttext">Sheffield Teaching Hospitals NHS Foundation Trust</a></h4>
+      </div>
+      <div class="barnsleybrand">
+        <h4 class="lighttext"><a href="#" class="lighttext">Sheffield Teaching Hospitals NHS Foundation Trust</a></h4>
+      </div>      
+    </div>
+    <div class="col-md-6" id="working-together-twitter-widget">
+      <a class="twitter-timeline"  href="https://twitter.com/SheffieldHosp/lists/working-together" data-widget-id="521649704682020866">Tweets from https://twitter.com/SheffieldHosp/lists/working-together</a>
+      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    </div>
+  </div>
+</div>
+
 
 			
 			<div id="content" class="clearfix row">
