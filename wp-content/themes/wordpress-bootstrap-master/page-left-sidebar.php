fc287bd4f5c7639108ec5555766e50b705e7d2ee
anotherangrymouse <grillopress.com@gmail.com>
2014-11-04 14:56:28 +0000

    create workstream template

--- wp-content/themes/wordpress-bootstrap-master/page-left-sidebar.php
+++ wp-content/themes/wordpress-bootstrap-master/page-left-sidebar.php
@@ -1,44 +1,57 @@
 <?php
 /*
-Template Name: Left Sidebar Page
+Template Name: Services page
 */
 ?>
-
 <?php get_header(); ?>
-<div class="container">
-			
+
 			<div id="content" class="clearfix row">
-            
-            	
-			
-				<div id="main" class="col col-lg-8 clearfix" role="main">
+        
+        <div class="row">
+          <div class="col-md-12">
+            <ol class="breadcrumb hidden-xs">
+            <?php if ( function_exists('yoast_breadcrumb') ) {
+            yoast_breadcrumb('<li id="breadcrumbs">','</li>');
+            } ?>
+            </ol>
+            <br>
+            <br>
+            <h1 class="brand-color workstream-h1 lighttext"><?php the_title(); ?></h1>
+            <?php echo '<p class="lead">' . get_post_meta($post->ID, 'workstream_desc', true) . '</p>'; ?>
+          </div>
+        </div>
+        
+        <div class="row">
+          				<div id="main" class="col-sm-8 clearfix" role="main">
+          
+        
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
+					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 						
 						<header>
 							
-							<div class="page-header"><h1><?php the_title(); ?></h1></div>
+							
 						
 						</header> <!-- end article header -->
 					
-						<section class="post_content">
+						<section class="post_content clearfix" itemprop="articleBody">
 							<?php the_content(); ?>
 					
 						</section> <!-- end article section -->
 						
 						<footer>
 			
-							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","wpbootstrap") . ': ', ', ', '</span>'); ?></p>
+							<?php the_tags('<p class="tags"><span class="tags-title">' . __("Tags","wpbootstrap") . ':</span> ', ', ', '</p>'); ?>
 							
 						</footer> <!-- end article footer -->
 					
 					</article> <!-- end article -->
 					
-					<?php comments_template(); ?>
+					<?php comments_template('',true); ?>
 					
-					<?php endwhile; ?>	
+					<?php endwhile; ?>		
 					
 					<?php else : ?>
 					
@@ -57,7 +70,16 @@ Template Name: Left Sidebar Page
 			
 				</div> <!-- end #main -->
     
+				<?php get_sidebar(); // sidebar 1 ?>
+    
 			</div> <!-- end #content -->
+        </div>
+        
+        
+        
+        
+        
+
 
 </div> <!-- end #container -->
 
